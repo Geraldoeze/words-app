54493165848c66999bb87c4dcb23897eb94e653e
Gerald Ezenagu <egerald344@gmail.com>
2023-10-06 22:46:08 +0100

    code2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import clientPromise from "../../lib/mongodb";
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    const client = clientPromise;
-    const db = client.db("words");
-    const dataFromDb = db.collection("alphabets").find({}).toArray();
-    res.status(200).json({ message: "test POST", res: dataFromDb });
-  } else {
-    const client = clientPromise;
-    const db = client.db("words");
-    const dataFromDb = db.collection("alphabets").find({}).toArray();
 
-    res
-      .status(200)
-      .json({ message: "test working for other methods", res: dataFromDb });
-  }
+export default function handler(req, res) {
+  res
+    .status(200)
+    .json({ message: "test working for other methods", res: dataFromDb });
 }
