9f78346e87d7f2dd5107b0ecf9af0d1b1a8325e4
Gerald Ezenagu <egerald344@gmail.com>
2023-10-18 22:04:40 +0100

    test-Code

--- utils/http-request.js
+++ utils/http-request.js
@@ -2,13 +2,11 @@ import axios from "axios";
 let uri;
 
 if (process.env.REACT_APP_ENVIRONMENT == "development") {
-  // uri = 'https://words-server.vercel.app/server'
-  uri = "http://localhost:7000";
+  uri = "https://words-server.vercel.app/server";
 } else if (process.env.REACT_APP_ENVIRONMENT == "production") {
-  // uri = 'https://words-server.vercel.app/server';
+  uri = "https://words-server.vercel.app/server";
 } else {
-    uri = "http://localhost:7000/server";
-  // uri = 'https://words-server.vercel.app/server';
+  uri = "https://words-server.vercel.app/server";
 }
 console.log(uri);
 const Axios = axios.create({ baseURL: uri });
